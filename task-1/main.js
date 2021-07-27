6548e40bb91d2fe11f63ff80413a1a541d1a90a5
AtulKarn <atkkul@gmail.com>
2021-07-28 01:21:48 +0545

    image gallary added

--- task-1/main.js
+++ task-1/main.js
@@ -47,3 +47,31 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
   showSlidesauto();
 }
+
+
+function currentSlideGallary(n) {
+  showSlidesGallary((slideIndex = n));
+}
+
+function showSlidesGallary(n) {
+  var i;
+  var slides = document.getElementsByClassName("imageslide");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+
+
+
